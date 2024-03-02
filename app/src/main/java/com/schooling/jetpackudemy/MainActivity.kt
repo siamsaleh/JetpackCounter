7c71eae12320627a97fee7dd5b2f493139e97ab8
SiamSaleh <siamsaleh9@gmail.com>
2024-03-02 18:48:12 +0600

    Header Text Input Created

--- app/src/main/java/com/schooling/jetpackudemy/MainActivity.kt
+++ app/src/main/java/com/schooling/jetpackudemy/MainActivity.kt
@@ -1,59 +1,140 @@
 package com.schooling.jetpackudemy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.schooling.jetpackudemy.components.InputField
 import com.schooling.jetpackudemy.ui.theme.JetpackUdemyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackUdemyTheme {
-                MyApp()
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
-
-@Preview(showSystemUi = true, device = "id:pixel_5")
 @Composable
-fun MyApp() {
+fun MyApp(content: @Composable () -> Unit) {
+    JetpackUdemyTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun TopHeader(totalPerPerson: Double = 0.0) {
     Surface(
         modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(),
-        color = Color.Blue
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(12.dp))),
+        color = Color(0xFFE9D7F7)
     ) {
+        Column(
+            modifier = Modifier
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total Per Person",
+                style = MaterialTheme.typography.headlineSmall
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
 
+@Preview
+@Composable
+fun MainContent() {
+    BillForm(){billAmt ->
+        Log.d("AMT", "MAin Content: $billAmt")
+    }
+}
+
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {}
+) {
+    val totalBillState = remember {
+        mutableStateOf("0")
+    }
+
+    val validState = remember(totalBillState.value) {
+        // Here we trim totalBillState.value and check it is empty or not
+        totalBillState.value.trim().isNotEmpty()
+    }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Surface(
+        modifier = Modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter Bill",
+                enabled = true,
+                isSingleLine = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    // Todo - onValueChanged
+                    onValChange(totalBillState.value.trim())
+
+                    keyboardController?.hide()
+                }
+            )
+        }
+    }
+}
+
+
+/*@Preview(showSystemUi = true, device = "id:pixel_5")*/
+@Composable
+fun Preview() {
+    MyApp {
+        TopHeader()
     }
 }

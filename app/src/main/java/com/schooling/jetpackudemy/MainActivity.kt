f10452ea24af2c03e82d35b0d6854250e3bf5b5c
SiamSaleh <siamsaleh9@gmail.com>
2024-03-02 13:24:35 +0600

    Money Counter Added

--- app/src/main/java/com/schooling/jetpackudemy/MainActivity.kt
+++ app/src/main/java/com/schooling/jetpackudemy/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.schooling.jetpackudemy
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -18,25 +16,18 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,7 +48,7 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi = true, device = "id:pixel_5")
 @Composable
 fun MyApp() {
-    var moneyCounter by remember {
+    val moneyCounter = remember {
         mutableIntStateOf(0)
     }
     Surface(
@@ -70,19 +61,27 @@ fun MyApp() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "$100", style = TextStyle(
+            Text(text = "$${moneyCounter.intValue}", style = TextStyle(
                 fontSize = 39.sp),
                 color = Color.White,
                 fontWeight = FontWeight.ExtraBold
             )
             Spacer(modifier = Modifier.height(30.dp))
-            CreateCircle()
+            CreateCircle(moneyCounter = moneyCounter.intValue){newValue ->
+                moneyCounter.intValue = newValue
+            }
+            if (moneyCounter.intValue > 25){
+                Text(text = "Lots of Money", style = TextStyle(
+                    fontSize = 15.sp),
+                    color = Color.White
+                )
+            }
         }
     }
 }
 
 @Composable
-fun CreateCircle() {
+fun CreateCircle(moneyCounter: Int = 0, UpdateMoneyCounter: (Int) -> Unit) { // Unit is return nothing
     Card(
         modifier = Modifier
             .padding(3.dp)
@@ -94,6 +93,7 @@ fun CreateCircle() {
             modifier = Modifier
                 .fillMaxSize()
                 .clickable {
+                    UpdateMoneyCounter(moneyCounter + 1)
                 },
             contentAlignment = Alignment.Center
         ) {

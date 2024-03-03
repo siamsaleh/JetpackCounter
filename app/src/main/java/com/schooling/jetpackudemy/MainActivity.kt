b9a9abf13c0aed06c7d3fde5378c6f576033c47c
siamsaleh <siamsaleh9@gmail.com>
2024-03-03 22:19:02 +0600

    Starting Layout Create

--- app/src/main/java/com/schooling/jetpackudemy/MainActivity.kt
+++ app/src/main/java/com/schooling/jetpackudemy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.schooling.jetpackudemy
 
 import android.os.Bundle
+import android.support.v4.os.IResultReceiver2.Default
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -18,6 +19,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,67 +39,32 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackUdemyTheme {
-                MyApp()
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
 
-@Preview(showSystemUi = true, device = "id:pixel_5")
 @Composable
-fun MyApp() {
-    val moneyCounter = remember {
-        mutableIntStateOf(0)
-    }
-    Surface(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(),
-        color = Color.Blue
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "$${moneyCounter.intValue}", style = TextStyle(
-                fontSize = 39.sp),
-                color = Color.White,
-                fontWeight = FontWeight.ExtraBold
-            )
-            Spacer(modifier = Modifier.height(30.dp))
-            CreateCircle(moneyCounter = moneyCounter.intValue){newValue ->
-                moneyCounter.intValue = newValue
-            }
-            if (moneyCounter.intValue > 25){
-                Text(text = "Lots of Money", style = TextStyle(
-                    fontSize = 15.sp),
-                    color = Color.White
-                )
-            }
-        }
+fun MyApp(content: @Composable ()-> Unit) {
+    JetpackUdemyTheme {
+        content()
     }
 }
 
 @Composable
-fun CreateCircle(moneyCounter: Int = 0, UpdateMoneyCounter: (Int) -> Unit) { // Unit is return nothing
-    Card(
-        modifier = Modifier
-            .padding(3.dp)
-            .size(150.dp), shape = CircleShape,
-        elevation = CardDefaults.cardElevation(10.dp),
-        border = BorderStroke(1.dp, color = Color.Red)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .clickable {
-                    UpdateMoneyCounter(moneyCounter + 1)
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            Text(text = "Tap", fontSize = 25.sp, modifier = Modifier)
-        }
+fun MainContent(){
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Text(text = "Hello")
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreview(){
+    MyApp {
+        MainContent()
     }
 }

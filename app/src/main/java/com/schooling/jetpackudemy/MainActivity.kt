98edd76fe50295ad9dd4ac40fa87ad452598170c
siamsaleh <siamsaleh9@gmail.com>
2024-03-04 01:02:29 +0600

    Topbar and Scaffoled

--- app/src/main/java/com/schooling/jetpackudemy/MainActivity.kt
+++ app/src/main/java/com/schooling/jetpackudemy/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.schooling.jetpackudemy
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -23,23 +35,48 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun MyApp(content: @Composable ()-> Unit) {
+fun MyApp(content: @Composable () -> Unit) {
     JetpackUdemyTheme {
-        content()
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    modifier = Modifier
+                        .shadow(
+                            elevation = 5.dp
+                        ),
+                    title = { Text(text = "Movie App") },
+                    colors = topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary
+                    )
+                )
+            },
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                content()
+            }
+        }
     }
 }
 
 @Composable
-fun MainContent(){
+fun MainContent() {
     Surface(color = MaterialTheme.colorScheme.background) {
-        Text(text = "Hello")
+        Text(text = "Hello", fontSize = 20.sp)
     }
 }
 
 @Preview
 @Composable
-fun DefaultPreview(){
+fun DefaultPreview() {
     MyApp {
         MainContent()
     }
